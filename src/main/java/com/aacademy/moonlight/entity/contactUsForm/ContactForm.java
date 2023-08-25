d05f1e17b4aeb39111e575b4c44b1174ef1f1a0c
Anton1892Ivanov <133897731+Anton1892Ivanov@users.noreply.github.com>
2023-08-25 11:20:42 +0300

    JB23AT2-6-Create-Contact-Us-Form-entity (#8)
    * JB23AT2-6-Create-Contact-Us-Form-entity
    
    * JB23AT2-6 Create Contact Form
    
    * JB23AT2-6 Create Contact Form
    
    * remove not required properties
    
    ---------
    
    Co-authored-by: dermenji <alex.dermenji@gmail.com>

---
+++ src/main/java/com/aacademy/moonlight/entity/contactUsForm/ContactForm.java
@@ -0,0 +1,42 @@
+package com.aacademy.moonlight.entity.contactUsForm;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Table(name = "contact_forms")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ContactForm {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    @NotEmpty
+    @Size(min = 2, max = 255, message = "First name length must be between {min} and {max} characters")
+    private String userName;
+
+    @Column(name = "user_email", nullable = false)
+    @Email(message = "Email must be valid")
+    @NotNull
+    private String userEmail;
+
+    @Column(name = "user_phone_number", nullable = false)
+    @NotEmpty(message = "Enter your phone number")
+    @Pattern(regexp = "^(\\+|00)[0-9\\-]{10,15}$")
+    @Size(min = 10, max = 15)
+    private String userPhoneNumber;
+
+    @Column(name = "message", nullable = false)
+    @NotNull
+    @NotBlank(message = "You have to write your comment")
+    private String message;
+
+
+}
