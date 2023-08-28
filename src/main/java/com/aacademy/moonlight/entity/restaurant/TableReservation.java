0076dd90020b8c7dc7b3631f6abe5bb297f03f36
epxanr <133875257+epxanr@users.noreply.github.com>
2023-08-28 12:01:54 +0300

    Jb23 at2 5 create restaurant and all related entities (#7)
    * test
    
    * Created classes for Restaurant.
    
    * Added annotations.
    
    * Fixed some things.
    
    * Swapped @Column for @JoinColumn
    and made the payment status boolean.
    
    * Swapped @Column for @JoinColumn
    and made the payment status boolean.
    
    * reservations ---> table_reservations
    
    * adjust some naming
    
    * Please do not forget to revert properties
    
    ---------
    
    Co-authored-by: Georgi Pachilov <g_pachilov@yahoo.com>
    Co-authored-by: dermenji <alex.dermenji@gmail.com>

---
+++ src/main/java/com/aacademy/moonlight/entity/restaurant/TableReservation.java
@@ -0,0 +1,59 @@
+package com.aacademy.moonlight.entity.restaurant;
+
+
+import com.aacademy.moonlight.entity.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "table_reservations")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "date")
+    @FutureOrPresent(message = "Date must be in the future")
+    @NotNull
+    private LocalDate date;
+
+    @Column(name = "hour")
+    @NotNull
+    private LocalTime hour;
+
+    @Column(name = "count_people")
+    @Min(value = 1)
+    @Max(value = 11)
+    @NotNull(message = "Please select the number of people")
+    private Integer countPeople;
+
+    @Column(name = "total_price")
+    @Min(value = 10)
+    @NotNull(message = "Missing price")
+    private Double totalPrice;
+
+    @JoinColumn(name = "table_number")
+    @ManyToOne
+    private TableRestaurant tableRestaurant;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private User User;
+
+    @Column(name = "payment_status")
+    private boolean paymentStatus;
+
+}
+
---
+++ src/main/java/com/aacademy/moonlight/entity/restaurant/TableZone.java
@@ -0,0 +1,8 @@
+package com.aacademy.moonlight.entity.restaurant;
+
+public enum TableZone {
+
+    BAR,
+    SALOON,
+    TERRACE
+}
