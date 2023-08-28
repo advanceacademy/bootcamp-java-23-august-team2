e30da436a8b2d07b03ad797848c09c3f0ec3901f
Daniela Petrova <dani055@abv.bg>
2023-08-28 20:47:51 +0300

    JB23AT2-11-Add-repository-layer-for-Hotel-and-related-addedAnnotations

--- src/main/java/com/aacademy/moonlight/repository/hotel/RoomFacilityRepository.java
+++ src/main/java/com/aacademy/moonlight/repository/hotel/RoomFacilityRepository.java
@@ -2,6 +2,8 @@ package com.aacademy.moonlight.repository.hotel;
 
 import com.aacademy.moonlight.entity.hotel.RoomFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomFacilityRepository extends JpaRepository<RoomFacility, Long> {
 }
