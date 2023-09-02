7edc800681ae55c23f55c884b96c3b796368a2de
Ahmed Mustafov <135149550+AhmedMustafov@users.noreply.github.com>
2023-09-02 13:56:57 +0300

    JB23AT2-4-Create-Screen-and-all-related-entities (#13)
    * JB23AT2-4-Create-Screen-and-all-related-entities
    
    * JB23AT2-4-Create-Screen-and-all-related-entities - added user relations and  some annotations

---
+++ src/main/java/com/aacademy/moonlight/entity/bar/ScreenEvent.java
@@ -0,0 +1,39 @@
+package com.aacademy.moonlight.entity.bar;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "screen_events")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ScreenEvent {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "event")
+    @NotEmpty(message = "Enter event name")
+    private String event;
+
+    @Column(name = "event_date")
+    @NotNull(message = "Enter date")
+    @Future
+    private LocalDate date;
+
+    @OneToOne
+    @JoinColumn(name = "screen_id")
+    private BarScreen barScreen;
+}
---
+++ src/main/java/com/aacademy/moonlight/entity/bar/ScreenSeat.java
@@ -0,0 +1,30 @@
+package com.aacademy.moonlight.entity.bar;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Entity
+@Table(name = "screen_seats")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ScreenSeat {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "position" , unique = true)
+    @NotEmpty(message = "Position is required")
+    private String position;
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "bar_screen_id")
+    private BarScreen barScreen;
+}
