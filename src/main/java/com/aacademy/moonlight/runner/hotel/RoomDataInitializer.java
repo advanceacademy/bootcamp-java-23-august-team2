64056adcfcc4ba16fdd0f2c89c53a224bebcc2f9
AlexLazrag <alex.lazrag@gmail.com>
2023-09-06 15:30:57 +0300

    Added roomDataInit. Created all rooms and saved to repo. TODO - make room size in entity

---
+++ src/main/java/com/aacademy/moonlight/runner/hotel/RoomDataInitializer.java
@@ -0,0 +1,223 @@
+package com.aacademy.moonlight.runner.hotel;
+
+import com.aacademy.moonlight.entity.hotel.Room;
+import com.aacademy.moonlight.entity.hotel.RoomFacility;
+import com.aacademy.moonlight.entity.hotel.RoomType;
+import com.aacademy.moonlight.entity.hotel.RoomView;
+import com.aacademy.moonlight.repository.hotel.RoomFacilityRepository;
+import com.aacademy.moonlight.repository.hotel.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RoomDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    RoomFacilityRepository roomFacilityRepository;
+
+    @Autowired
+    public RoomDataInitializer(RoomRepository roomRepository, RoomFacilityRepository roomFacilityRepository) {
+        this.roomRepository = roomRepository;
+        this.roomFacilityRepository = roomFacilityRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RoomFacility lounge = RoomFacility.builder()
+                .facility("Lounge area")
+                .build();
+
+        RoomFacility TV = RoomFacility.builder()
+                .facility("Flat screen TV")
+                .build();
+
+        RoomFacility phone = RoomFacility.builder()
+                .facility("Telephone")
+                .build();
+
+        RoomFacility toiletries = RoomFacility.builder()
+                .facility("Toiletries")
+                .build();
+
+        RoomFacility kettle = RoomFacility.builder()
+                .facility("Electric kettle")
+                .build();
+
+        RoomFacility internet = RoomFacility.builder()
+                .facility("Wired and wireless internet")
+                .build();
+
+        RoomFacility mattress = RoomFacility.builder()
+                .facility("Comfortable mattress")
+                .build();
+
+        RoomFacility minibar = RoomFacility.builder()
+                .facility("Minibar")
+                .build();
+
+        RoomFacility safe = RoomFacility.builder()
+                .facility("Safe")
+                .build();
+
+        RoomFacility hairDryer = RoomFacility.builder()
+                .facility("HairDryer")
+                .build();
+
+        RoomFacility terrace = RoomFacility.builder()
+                .facility("Terrace")
+                .build();
+
+        RoomFacility spaAccessories = RoomFacility.builder()
+                .facility("Bathrobe and spa slippers")
+                .build();
+
+        RoomFacility sofaBed = RoomFacility.builder()
+                .facility("Sofa bed")
+                .build();
+
+        List<RoomFacility> standardRoomFacilities = List.of(TV, phone, toiletries, kettle, internet, mattress, minibar, safe, hairDryer, spaAccessories);
+        List<RoomFacility> studioRoomFacilities = List.of(TV, phone, toiletries, kettle, internet, mattress, minibar, safe, hairDryer, spaAccessories, sofaBed);
+        List<RoomFacility> apartmentRoomFacilities = List.of(lounge, TV, phone, toiletries, kettle, internet, mattress, minibar, safe, hairDryer, terrace, spaAccessories);
+
+        roomFacilityRepository.saveAll(standardRoomFacilities);
+        roomFacilityRepository.saveAll(studioRoomFacilities);
+        roomFacilityRepository.saveAll(apartmentRoomFacilities);
+
+//TODO ADD ROOM SIZES IN ENTITY
+        Room.RoomBuilder standardRoomBuilder = Room.builder()
+                .price(220.0)
+                .type(RoomType.STANDARD)
+                .facilities(standardRoomFacilities);
+
+        Room.RoomBuilder studioRoomBuilder = Room.builder()
+                .price(320.0)
+                .type(RoomType.STUDIO)
+                .facilities(studioRoomFacilities);
+
+        Room.RoomBuilder apartmentRoomBuilder = Room.builder()
+                .price(520.0)
+                .type(RoomType.APARTMENT)
+                .facilities(apartmentRoomFacilities);
+
+        //standard rooms
+        Room standardRoom1 = standardRoomBuilder
+                .id(1L)
+                .roomNumber(1)
+                .view(RoomView.SEA)
+                .build();
+
+        Room standardRoom2 = standardRoomBuilder
+                .id(2L)
+                .roomNumber(2)
+                .view(RoomView.SEA)
+                .build();
+
+        Room standardRoom3 = standardRoomBuilder
+                .id(3L)
+                .roomNumber(3)
+                .view(RoomView.POOL)
+                .build();
+
+        Room standardRoom4 = standardRoomBuilder
+                .id(4L)
+                .roomNumber(4)
+                .view(RoomView.POOL)
+                .build();
+
+        Room standardRoom5 = standardRoomBuilder
+                .id(5L)
+                .roomNumber(5)
+                .view(RoomView.GARDEN)
+                .build();
+
+        Room standardRoom6 = standardRoomBuilder
+                .id(6L)
+                .roomNumber(6)
+                .view(RoomView.GARDEN)
+                .build();
+
+        Room standardRoom7 = standardRoomBuilder
+                .id(7L)
+                .roomNumber(7)
+                .view(RoomView.GARDEN)
+                .build();
+
+        Room standardRoom8 = standardRoomBuilder
+                .id(8L)
+                .roomNumber(8)
+                .view(RoomView.GARDEN)
+                .build();
+
+        //studio rooms
+        Room studioRoom1 = studioRoomBuilder
+                .id(9L)
+                .roomNumber(9)
+                .view(RoomView.SEA)
+                .build();
+
+        Room studioRoom2 = studioRoomBuilder
+                .id(10L)
+                .roomNumber(10)
+                .view(RoomView.SEA)
+                .build();
+
+        Room studioRoom3 = studioRoomBuilder
+                .id(11L)
+                .roomNumber(11)
+                .view(RoomView.POOL)
+                .build();
+
+        Room studioRoom4 = studioRoomBuilder
+                .id(12L)
+                .roomNumber(12)
+                .view(RoomView.POOL)
+                .build();
+
+        Room studioRoom5 = studioRoomBuilder
+                .id(13L)
+                .roomNumber(13)
+                .view(RoomView.GARDEN)
+                .build();
+
+        Room studioRoom6 = studioRoomBuilder
+                .id(14L)
+                .roomNumber(14)
+                .view(RoomView.GARDEN)
+                .build();
+
+        //apartments
+        Room apartment1 = apartmentRoomBuilder
+                .id(15L)
+                .roomNumber(15)
+                .view(RoomView.SEA)
+                .build();
+
+        Room apartment2 = apartmentRoomBuilder
+                .id(16L)
+                .roomNumber(16)
+                .view(RoomView.SEA)
+                .build();
+
+        Room apartment3 = apartmentRoomBuilder
+                .id(17L)
+                .roomNumber(17)
+                .view(RoomView.POOL)
+                .build();
+
+        List<Room> standardRooms = List.of(standardRoom1, standardRoom2, standardRoom3, standardRoom4, standardRoom5, standardRoom6, standardRoom7, standardRoom8);
+        List<Room> studioRooms = List.of(studioRoom1, studioRoom2, studioRoom3, studioRoom4, studioRoom5, studioRoom6);
+        List<Room> apartmentRooms = List.of(apartment1, apartment2, apartment3);
+
+        roomRepository.saveAll(standardRooms);
+        roomRepository.saveAll(studioRooms);
+        roomRepository.saveAll(apartmentRooms);
+
+    }
+}
