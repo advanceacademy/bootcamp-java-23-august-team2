bc47b198883d5eefe6c056588fb0490c19ee07d6
Nikola Grigorov <grigorov.ny@gmail.com>
2023-09-11 16:41:24 +0300

    createUserRole and createUser methods created in the AddAdminAndClientRunner.java
    Password encryption remains
    
    JB23AT2-21-Add-assets-for-User-roles

--- src/main/java/com/aacademy/moonlight/repository/user/UserRoleRepository.java
+++ src/main/java/com/aacademy/moonlight/repository/user/UserRoleRepository.java
@@ -1,5 +1,6 @@
 package com.aacademy.moonlight.repository.user;
 
+import com.aacademy.moonlight.entity.user.User;
 import com.aacademy.moonlight.entity.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
+    Optional<UserRole> findByUserRole(String userRole);
 }
