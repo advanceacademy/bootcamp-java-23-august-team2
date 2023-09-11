31ef783d0bdfbeb75186e97ac01b13c2c4127c6c
Nikola Grigorov <grigorov.ny@gmail.com>
2023-09-11 17:27:57 +0300

    added password encryption. New class SecurityConfig created. In PasswordConstraintValidator.java changed the maximum size to 255 Added security dependency in the pom.xml
    JB23AT2-21-Add-assets-for-User-roles

--- src/main/java/com/aacademy/moonlight/runner/AddAdminAndClientRunner.java
+++ src/main/java/com/aacademy/moonlight/runner/AddAdminAndClientRunner.java
@@ -3,12 +3,13 @@ package com.aacademy.moonlight.runner;
 
 import com.aacademy.moonlight.dto.user.UserRequest;
 import com.aacademy.moonlight.dto.user.UserRoleRequest;
-import com.aacademy.moonlight.entity.user.UserRole;
 import com.aacademy.moonlight.repository.user.UserRepository;
 import com.aacademy.moonlight.repository.user.UserRoleRepository;
 import com.aacademy.moonlight.service.user.UserRoleService;
 import com.aacademy.moonlight.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,6 +22,10 @@ public class AddAdminAndClientRunner implements CommandLineRunner {
     private final UserRoleService userRoleService;
 
     private final UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     public AddAdminAndClientRunner(UserRoleRepository userRoleRepository, UserRepository userRepository, UserRoleService userRoleService, UserService userService) {
         this.userRoleRepository = userRoleRepository;
@@ -38,8 +43,6 @@ public class AddAdminAndClientRunner implements CommandLineRunner {
 
             createUser("vetko.manqka@gmail.com");
 
-
-
     }
 
     private void createUserRole(String userRole) {
@@ -50,14 +53,14 @@ public class AddAdminAndClientRunner implements CommandLineRunner {
             userRoleService.createUserRole(userRoleRequest);
         }
     }
-    // TO DO the rest  +
+
     private void createUser (String email) {
         UserRequest userRequest =  UserRequest.builder()
                 .email(email)
                 .firstName("Vetko")
                 .lastName("Arabadjiev")
                 .phoneNumber("+359892222222")
-                .password("Adm!n54321")
+                .password(bCryptPasswordEncoder.encode("Adm!n54321"))
                 .userRole(userRoleRepository.findByUserRole("admin").orElseThrow())
                 .build();
 
