ad721f755776fe3efafc46d7a3b49eb112181e33
AlexLazrag <alex.lazrag@gmail.com>
2023-09-12 15:34:17 +0300

    Trying to resolve merge problems. Commit service layer again.

---
+++ src/main/java/com/aacademy/moonlight/service/car/impl/CarCategoryServiceImpl.java
@@ -0,0 +1,47 @@
+package com.aacademy.moonlight.service.car.impl;
+
+import com.aacademy.moonlight.converter.car.CarCategoryConverter;
+import com.aacademy.moonlight.dto.car.CarCategoryRequest;
+import com.aacademy.moonlight.entity.car.CarCategory;
+import com.aacademy.moonlight.repository.car.CarCategoryRepository;
+import com.aacademy.moonlight.service.car.CarCategoryService;
+
+public class CarCategoryServiceImpl implements CarCategoryService {
+    private final CarCategoryRepository repository;
+    private final CarCategoryConverter converter;
+
+    public CarCategoryServiceImpl(CarCategoryRepository repository, CarCategoryConverter converter) {
+        this.repository = repository;
+        this.converter = converter;
+    }
+
+    @Override
+    public CarCategory saveCarCategory(CarCategoryRequest request) {
+        CarCategory carCategory = converter.toCarCategory(request);
+        return repository.save(carCategory);
+    }
+
+    @Override
+    public CarCategory getCarCategoryById(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Car category with this id not found.")
+        );
+    }
+
+    @Override
+    public CarCategory updateCarCategory(CarCategoryRequest request, Long id) {
+        CarCategory carCategory = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Car category not found.")
+        );
+        carCategory.setType(request.getType());
+        carCategory.setSeats(request.getSeats());
+        carCategory.setPrice(request.getPrice());
+
+        return repository.save(carCategory);
+    }
+
+    @Override
+    public void deleteCarCategoryById(Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/aacademy/moonlight/service/car/impl/CarServiceImpl.java
@@ -0,0 +1,51 @@
+package com.aacademy.moonlight.service.car.impl;
+
+import com.aacademy.moonlight.converter.car.CarConverter;
+import com.aacademy.moonlight.dto.car.CarRequest;
+import com.aacademy.moonlight.entity.car.Car;
+import com.aacademy.moonlight.repository.car.CarRepository;
+import com.aacademy.moonlight.service.car.CarService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final CarConverter converter;
+    private final CarRepository repository;
+
+    public CarServiceImpl(CarConverter converter, CarRepository repository) {
+        this.converter = converter;
+        this.repository = repository;
+    }
+
+    @Override
+    public Car saveCar(CarRequest request) {
+        Car car = converter.toCar(request);
+        return repository.save(car);
+    }
+
+    @Override
+    public Car getCarById(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Car with this id not found")
+        );
+    }
+
+    @Override
+    public Car updateCar(CarRequest request, Long id) {
+        Car car = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Car not found")
+        );
+        car.setBrand(request.getBrand());
+        car.setModel(request.getModel());
+        car.setManufacturingYear(request.getManufacturingYear());
+        car.setCarCategory(request.getCarCategory());
+        car.setFileResourcesForCar(request.getFileResourcesForCar());
+
+        return repository.save(car);
+    }
+
+    @Override
+    public void deleteCarById(Long id) {
+        repository.deleteById(id);
+    }
+}
