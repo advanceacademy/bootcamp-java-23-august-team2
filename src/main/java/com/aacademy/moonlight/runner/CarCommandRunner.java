8a35f6c00bc2bf84bc735458cdf6194a733b8da9
AlexLazrag <alex.lazrag@gmail.com>
2023-09-20 17:18:25 +0300

    Cleaned code and set the list of images in teh cars

--- src/main/java/com/aacademy/moonlight/runner/CarCommandRunner.java
+++ src/main/java/com/aacademy/moonlight/runner/CarCommandRunner.java
@@ -42,78 +42,85 @@ public class CarCommandRunner implements CommandLineRunner {
         CarCategory sedanCategory = createCategory(CarType.SEDAN, 5, 800.0);
         CarCategory vanCategory = createCategory(CarType.VAN, 8, 600.0);
 
-        Car ferrariF8 = createCarWithImages("Ferrari", "F8", 2021, sportsCategory);
-        List<FileResourcesForCar> ferrariF8List = new ArrayList<>();
-        addImage("ferrariF8-1.jpg", "/ferrariF8/ferrariF8Photo-1.jpg", ferrariF8, ferrariF8List);
-        addImage("ferrariF8-2.jpg", "/ferrariF8/ferrariF8Photo-2.jpg", ferrariF8, ferrariF8List);
-        addImage("ferrariF8-3.jpg", "/ferrariF8/ferrariF8Photo-3.jpg", ferrariF8, ferrariF8List);
+        Car ferrariF8 = createCarWithImages("Ferrari", "F8", 2021, sportsCategory,
+                "ferrariF8-1.jpg", "/ferrariF8/ferrariF8Photo-1.jpg",
+                "ferrariF8-2.jpg", "/ferrariF8/ferrariF8Photo-2.jpg",
+                "ferrariF8-3.jpg", "/ferrariF8/ferrariF8Photo-3.jpg"
+        );
 
-        Car lamborghiniUrus = createCarWithImages("Lamborghini", "Urus", 2020, sportsCategory);
-        List<FileResourcesForCar> lamborghiniUrusList = new ArrayList<>();
-        addImage("lamborghiniUrusPhoto-1.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-1.jpg", lamborghiniUrus, lamborghiniUrusList);
-        addImage("lamborghiniUrusPhoto-2.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-2.jpg", lamborghiniUrus, lamborghiniUrusList);
-        addImage("lamborghiniUrusPhoto-3.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-3.jpg", lamborghiniUrus, lamborghiniUrusList);
+        Car lamborghiniUrus = createCarWithImages("Lamborghini", "Urus", 2020, sportsCategory,
+                "lamborghiniUrusPhoto-1.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-1.jpg",
+                "lamborghiniUrusPhoto-2.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-2.jpg",
+                "lamborghiniUrusPhoto-3.jpg", "/lamborghiniUrus/lamborghiniUrusPhoto-3.jpg"
+        );
 
-        Car audiR8 = createCarWithImages("Audi", "R8", 2021, sportsCategory);
-        List<FileResourcesForCar> audiR8List = new ArrayList<>();
-        addImage("audiR8Photo-1.jpg", "/audiR8/audiR8Photo-1.jpg", audiR8, audiR8List);
-        addImage("audiR8Photo-2.jpg", "/audiR8/audiR8Photo-2.jpg", audiR8, audiR8List);
-        addImage("audiR8Photo-3.jpg", "/audiR8/audiR8Photo-3.jpg", audiR8, audiR8List);
+        Car audiR8 = createCarWithImages("Audi", "R8", 2021, sportsCategory,
+                "audiR8Photo-1.jpg", "/audiR8/audiR8Photo-1.jpg",
+                "audiR8Photo-2.jpg", "/audiR8/audiR8Photo-2.jpg",
+                "audiR8Photo-3.jpg", "/audiR8/audiR8Photo-3.jpg"
+        );
 
-        Car mercedesSClass = createCarWithImages("Mercedes", "S-Class", 2021, sedanCategory);
-        List<FileResourcesForCar> mercedesSClassList = new ArrayList<>();
-        addImage("mercedesSClassPhoto-1.jpg", "/mercedesSClass/mercedesSClassPhoto-1.jpg", mercedesSClass, mercedesSClassList);
-        addImage("mercedesSClassPhoto-2.jpg", "/mercedesSClass/mercedesSClassPhoto-2.jpg", mercedesSClass, mercedesSClassList);
-        addImage("mercedesSClassPhoto-3.jpg", "/mercedesSClass/mercedesSClassPhoto-3.jpg", mercedesSClass, mercedesSClassList);
+        Car mercedesSClass = createCarWithImages("Mercedes", "S-Class", 2021, sedanCategory,
+                "mercedesSClassPhoto-1.jpg", "/mercedesSClass/mercedesSClassPhoto-1.jpg",
+                "mercedesSClassPhoto-2.jpg", "/mercedesSClass/mercedesSClassPhoto-2.jpg",
+                "mercedesSClassPhoto-3.jpg", "/mercedesSClass/mercedesSClassPhoto-3.jpg"
+        );
 
-        Car audiA8 = createCarWithImages("Audi", "A8", 2021, sedanCategory);
-        List<FileResourcesForCar> audiA8List = new ArrayList<>();
-        addImage("audiA8Photo-1.jpg", "/audiA8/audiA8Photo-1.jpg", audiA8, audiA8List);
-        addImage("audiA8Photo-2.jpg", "/audiA8/audiA8Photo-2.jpg", audiA8, audiA8List);
-        addImage("audiA8Photo-3.jpg", "/audiA8/audiA8Photo-3.jpg", audiA8, audiA8List);
+        Car audiA8 = createCarWithImages("Audi", "A8", 2021, sedanCategory,
+                "audiA8Photo-1.jpg", "/audiA8/audiA8Photo-1.jpg",
+                "audiA8Photo-2.jpg", "/audiA8/audiA8Photo-2.jpg",
+                "audiA8Photo-3.jpg", "/audiA8/audiA8Photo-3.jpg"
+        );
 
-        Car bmw5series = createCarWithImages("BMW", "5 Series", 2020, sedanCategory);
-        List<FileResourcesForCar> bmw5seriesList = new ArrayList<>();
-        addImage("bmw5seriesPhoto-1.jpg", "/bmw5series/bmw5seriesPhoto-1.jpg", bmw5series, bmw5seriesList);
-        addImage("bmw5seriesPhoto-2.jpg", "/bmw5series/bmw5seriesPhoto-2.jpg", bmw5series, bmw5seriesList);
-        addImage("bmw5seriesPhoto-3.jpg", "/bmw5series/bmw5seriesPhoto-3.jpg", bmw5series, bmw5seriesList);
+        Car bmw5series = createCarWithImages("BMW", "5 Series", 2020, sedanCategory,
+                "bmw5seriesPhoto-1.jpg", "/bmw5series/bmw5seriesPhoto-1.jpg",
+                "bmw5seriesPhoto-2.jpg", "/bmw5series/bmw5seriesPhoto-2.jpg",
+                "bmw5seriesPhoto-3.jpg", "/bmw5series/bmw5seriesPhoto-3.jpg"
+        );
 
-        Car vwPassatB9 = createCarWithImages("Volkswagen", "Passat B9", 2021, sedanCategory);
-        List<FileResourcesForCar> vwPassatB9List = new ArrayList<>();
-        addImage("vwPassatB9Photo-1.jpg", "/vwPassatB9/vwPassatB9Photo-1.jpg", vwPassatB9, vwPassatB9List);
-        addImage("vwPassatB9Photo-2.jpg", "/vwPassatB9/vwPassatB9Photo-2.jpg", vwPassatB9, vwPassatB9List);
-        addImage("vwPassatB9Photo-3.jpg", "/vwPassatB9/vwPassatB9Photo-3.jpg", vwPassatB9, vwPassatB9List);
+        Car vwPassatB9 = createCarWithImages("Volkswagen", "Passat B9", 2021, sedanCategory,
+                "vwPassatB9Photo-1.jpg", "/vwPassatB9/vwPassatB9Photo-1.jpg",
+                "vwPassatB9Photo-2.jpg", "/vwPassatB9/vwPassatB9Photo-2.jpg",
+                "vwPassatB9Photo-3.jpg", "/vwPassatB9/vwPassatB9Photo-3.jpg"
+        );
 
-        Car mercedesVClass = createCarWithImages("Mercedes", "V-Class", 2021, vanCategory);
-        List<FileResourcesForCar> mercedesVCLassList = new ArrayList<>();
-        addImage("mercedesVClassPhoto-1.jpg", "/mercedesVClass/mercedesVClassPhoto-1.jpg", mercedesVClass, mercedesVCLassList);
-        addImage("mercedesVClassPhoto-2.jpg", "/mercedesVClass/mercedesVClassPhoto-2.jpg", mercedesVClass, mercedesVCLassList);
-        addImage("mercedesVClassPhoto-3.jpg", "/mercedesVClass/mercedesVClassPhoto-3.jpg", mercedesVClass, mercedesVCLassList);
+        Car mercedesVClass = createCarWithImages("Mercedes", "V-Class", 2021, vanCategory,
+                "mercedesVClassPhoto-1.jpg", "/mercedesVClass/mercedesVClassPhoto-1.jpg",
+                "mercedesVClassPhoto-2.jpg", "/mercedesVClass/mercedesVClassPhoto-2.jpg",
+                "mercedesVClassPhoto-3.jpg", "/mercedesVClass/mercedesVClassPhoto-3.jpg"
+        );
 
-        Car vwRLine = createCarWithImages("Volkswagen", "R Line", 2020, vanCategory);
-        List<FileResourcesForCar> vwRLineList = new ArrayList<>();
-        addImage("vwRLinePhoto-1.jpg", "/vwRLine/vwRLinePhoto-1.jpg", vwRLine, vwRLineList);
-        addImage("vwRLinePhoto-2.jpg", "/vwRLine/vwRLinePhoto-2.jpg", vwRLine, vwRLineList);
-        addImage("vwRLinePhoto-3.jpg", "/vwRLine/vwRLinePhoto-3.jpg", vwRLine, vwRLineList);
+        Car vwRLine = createCarWithImages("Volkswagen", "R Line", 2020, vanCategory,
+                "vwRLinePhoto-1.jpg", "/vwRLine/vwRLinePhoto-1.jpg",
+                "vwRLinePhoto-2.jpg", "/vwRLine/vwRLinePhoto-2.jpg",
+                "vwRLinePhoto-3.jpg", "/vwRLine/vwRLinePhoto-3.jpg"
+        );
 
-        Car bmwVan = createCarWithImages("BMW", "2 Series Gran Tourer", 2020, vanCategory);
-        List<FileResourcesForCar> bmwVanList = new ArrayList<>();
-        addImage("bmwVanPhoto-1.jpg", "/bmwVan/bmwVanPhoto-1.jpg", bmwVan, bmwVanList);
-        addImage("bmwVanPhoto-2.jpg", "/bmwVan/bmwVanPhoto-2.jpg", bmwVan, bmwVanList);
-        addImage("bmwVanPhoto-3.jpg", "/bmwVan/bmwVanPhoto-3.jpg", bmwVan, bmwVanList);
+        Car bmwVan = createCarWithImages("BMW", "2 Series Gran Tourer", 2020, vanCategory,
+                "bmwVanPhoto-1.jpg", "/bmwVan/bmwVanPhoto-1.jpg",
+                "bmwVanPhoto-2.jpg", "/bmwVan/bmwVanPhoto-2.jpg",
+                "bmwVanPhoto-3.jpg", "/bmwVan/bmwVanPhoto-3.jpg"
+        );
 
-        Car hyundaiH1 = createCarWithImages("Hyundai", "H-1", 2020, vanCategory);
-        List<FileResourcesForCar> hyundaiH1List = new ArrayList<>();
-        addImage("hyundaiH1Photo-1.jpg", "/hyundaiH1/hyundaiH1Photo-1.jpg", hyundaiH1, hyundaiH1List);
-        addImage("hyundaiH1Photo-2.jpg", "/hyundaiH1/hyundaiH1Photo-2.jpg", hyundaiH1, hyundaiH1List);
-        addImage("hyundaiH1Photo-3.jpg", "/hyundaiH1/hyundaiH1Photo-3.jpg", hyundaiH1, hyundaiH1List);
+        Car hyundaiH1 = createCarWithImages("Hyundai", "H-1", 2020, vanCategory,
+                "hyundaiH1Photo-1.jpg", "/hyundaiH1/hyundaiH1Photo-1.jpg",
+                "hyundaiH1Photo-2.jpg", "/hyundaiH1/hyundaiH1Photo-2.jpg",
+                "hyundaiH1Photo-3.jpg", "/hyundaiH1/hyundaiH1Photo-3.jpg"
+        );
 
+        List<FileResourcesForCar> ferrariF8Photos = ferrariF8.getFileResourcesForCar();
 
+        for (FileResourcesForCar photo : ferrariF8Photos) {
+            System.out.println("Photo Name: " + photo.getImageName());
+        }
 
     }
 
+    private Car createCarWithImages(String brand, String model, Integer year, CarCategory carCategory, String... imageInfo) {
+        if (imageInfo.length % 2 != 0) {
+            throw new IllegalArgumentException("Image info should be provided as pairs of name and path.");
+        }
 
-    private Car createCarWithImages(String brand, String model, Integer year, CarCategory carCategory) {
         Car car = carService.findCarByBrandAndModel(brand, model);
 
         if (car == null) {
@@ -124,12 +131,23 @@ public class CarCommandRunner implements CommandLineRunner {
                     .carCategory(carCategory)
                     .build();
             carRepository.save(car);
+
+            List<FileResourcesForCar> imageList = new ArrayList<>();
+
+            for (int i = 0; i < imageInfo.length; i += 2) {
+                String imageName = imageInfo[i];
+                String imagePath = imageInfo[i + 1];
+                FileResourcesForCar image = addImage(imageName, imagePath, car);
+                imageList.add(image);
+            }
+
+            car.setFileResourcesForCar(imageList);
         }
 
         return car;
     }
 
-    private void addImage(String name, String path, Car car, List<FileResourcesForCar> imageList) throws IOException {
+    private FileResourcesForCar addImage(String name, String path, Car car) {
         FileResourcesForCar existingImage = fileResourcesForCarService.findFileByName(name);
 
         if (existingImage == null) {
@@ -140,13 +158,12 @@ public class CarCommandRunner implements CommandLineRunner {
                 byte[] imageData = ImageFileUtil.readImageDataFromFile("src/main/java/com/aacademy/moonlight/carImages" + path);
                 newImage.setPhoto(imageData);
                 newImage.setCar(car);
-                fileResourcesForCarRepository.save(newImage);
-                imageList.add(newImage);
+                return fileResourcesForCarRepository.save(newImage);
             } catch (IOException e) {
-                throw new IOException("Failed to read image data from file: " + path, e);
+                throw new RuntimeException("Failed to read image data from file: " + path, e);
             }
         } else {
-            imageList.add(existingImage);
+            return existingImage;
         }
     }
 
