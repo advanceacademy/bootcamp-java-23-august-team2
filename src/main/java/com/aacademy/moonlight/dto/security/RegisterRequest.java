5ebf90488c0752d22baa852f4803e54df9e300ed
Nino Tsenov <nino101@abv.bg>
2023-09-21 19:29:22 +0300

    JB23AT2-34-Auth-functionality - tested version

--- src/main/java/com/aacademy/moonlight/dto/security/RegisterRequest.java
+++ src/main/java/com/aacademy/moonlight/dto/security/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.aacademy.moonlight.dto.security;
 
 import com.aacademy.moonlight.entity.user.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +11,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(value = {"role"}, allowGetters = true, allowSetters = false)
 public class RegisterRequest {
     private String firstName;
     private String lastName;
     private String email;
+    private String phoneNumber;
     private String password;
     private UserRole role;
 }
