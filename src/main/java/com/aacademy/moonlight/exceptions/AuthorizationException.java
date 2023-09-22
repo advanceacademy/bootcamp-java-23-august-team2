dcf8373ef03f5dce2343871ada708162b53168a7
AlexLazrag <133897218+AlexLazrag@users.noreply.github.com>
2023-09-22 18:50:35 +0300

    Jb23 at2 33 create exceptions (#28)
    * Exceptions added: Accepted, Created, Found, NotFound.
    
    * Removed Exceptions
    
    * Added handlers: INTERNAL_SERVER_ERROR, NOT_FOUND, UNAUTHORIZED, FORBIDDEN
    
    * removed unnecessary variable

---
+++ src/main/java/com/aacademy/moonlight/exceptions/AuthorizationException.java
@@ -0,0 +1,7 @@
+package com.aacademy.moonlight.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
