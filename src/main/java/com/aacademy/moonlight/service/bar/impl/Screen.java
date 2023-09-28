da2dab766fb6e8e1bc0f5337b5508919635e7c60
AhmedMustafov <ahmedkenanov@gmail.com>
2023-09-28 18:05:08 +0300

    JB23AT2-14-Add-service-layer-for-Screen-and-related

---
+++ src/main/java/com/aacademy/moonlight/service/bar/impl/Screen.java
@@ -0,0 +1,7 @@
+package com.aacademy.moonlight.service.bar.impl;
+
+public class Screen {
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/aacademy/moonlight/service/bar/impl/ScreenController.java
@@ -0,0 +1,23 @@
+package com.aacademy.moonlight.service.bar.impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/screens")
+public class ScreenController {
+    private final ScreenService screenService;
+    @Autowired
+    public ScreenController(ScreenService screenService)
+    {this.screenService = screenService;}
+    @GetMapping("/{id}")
+    public Screen getScreen(@PathVariable Long id )
+    {return screenService.getScreenById(id);}
+    @GetMapping("/all")
+    public  Iterable<Screen> getAllScreens()
+    {return screenService.getAllScreens();}
+    @PostMapping("/add")
+    public Screen addScreen(@RequestBody Screen screen)
+    {return screenService.saveScreen(screen);}
+    @DeleteMapping("/{id}")
+    public void deleteScreen(@PathVariable Long id)
+    {screenService.deleteScreenById(id);}
+}
