42f3e692c40e999b9b15f6ce0a6575bba82784e4
Nino Tsenov <nino101@abv.bg>
2023-09-30 10:10:23 +0300

    JB23AT2-32-Get-Hotel-room make it possible to use services with security

--- src/main/java/com/aacademy/moonlight/controller/hotel/RoomController.java
+++ src/main/java/com/aacademy/moonlight/controller/hotel/RoomController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/room")
+@RequestMapping("/api/v1/auth/room")
 public class RoomController {
 
     @Autowired
@@ -25,30 +25,30 @@ public class RoomController {
      return ResponseEntity.status(HttpStatus.FOUND).body(roomService.getRoomById(id));
     }
 
-    @GetMapping("/{roomNumber}api/v1/find-room-by-room-number")
+    @GetMapping("/{roomNumber}/find-room-by-room-number")
     public ResponseEntity<List<RoomResponse>> findRoomByRoomNumber(@Valid @PathVariable Integer roomNumber){
         List<RoomResponse>list = roomService.findByRoomNumber(roomNumber);
         return ResponseEntity.status(HttpStatus.FOUND).body(list);
     }
 
-    @GetMapping("/api/v1/all-rooms")
+    @GetMapping("/all-rooms")
     public ResponseEntity<List<RoomResponse>> findAllRooms(){
         List<RoomResponse> list = roomService.getAllRooms();
         return ResponseEntity.status(HttpStatus.FOUND).body(list);
     }
 
-    @GetMapping("/{roomType}/api/v1/get-rooms-by-view")
+    @GetMapping("/{roomType}/get-rooms-by-view")
     public ResponseEntity<List<RoomResponse>> getRoomsByRoomType(@Valid @PathVariable RoomType roomType){
         List<RoomResponse> list = roomService.findByRoomType(roomType);
         return ResponseEntity.status(HttpStatus.FOUND).body(list);
     }
 
-    @GetMapping("/{roomView}/api/v1/get-rooms-by-view")
+    @GetMapping("/{roomView}/get-rooms-by-view")
     public ResponseEntity<List<RoomResponse>> getRoomsByRoomView(@Valid @PathVariable RoomView roomView){
         List<RoomResponse> list = roomService.findByRoomView(roomView);
         return  ResponseEntity.status(HttpStatus.FOUND).body(list);
     }
-    @GetMapping("/{roomPrice}/api/v1/get-rooms-by-roomPrice")
+    @GetMapping("/{roomPrice}get-rooms-by-roomPrice")
     public ResponseEntity<List<RoomResponse>> getRoomsByRoomPrice (@Valid @PathVariable Long roomPrice){
         List<RoomResponse> list = roomService.findByRoomPrice(roomPrice);
         return ResponseEntity.status(HttpStatus.FOUND).body(list);
