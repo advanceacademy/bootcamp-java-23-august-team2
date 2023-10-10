e5d913de204da60767fc1bf37318e151ed19af42
Nino Tsenov <nino101@abv.bg>
2023-10-10 21:46:13 +0300

    JB23AT2-36-Swagger - removing conflicts

--- src/main/java/com/aacademy/moonlight/MoonlightApplication.java
+++ src/main/java/com/aacademy/moonlight/MoonlightApplication.java
@@ -2,10 +2,7 @@ package com.aacademy.moonlight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 @SpringBootApplication()
-
 public class MoonlightApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoonlightApplication.class, args);
