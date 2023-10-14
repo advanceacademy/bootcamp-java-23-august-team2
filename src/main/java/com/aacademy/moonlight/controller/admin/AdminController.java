a1dd9bf0a71d2876dd1b7953df3ad28acd20758d
Nino Tsenov <nino101@abv.bg>
2023-10-14 13:14:46 +0300

    JB23AT2-51-List-all-room-reservations

--- src/main/java/com/aacademy/moonlight/controller/admin/AdminController.java
+++ src/main/java/com/aacademy/moonlight/controller/admin/AdminController.java
@@ -3,6 +3,8 @@ package com.aacademy.moonlight.controller.admin;
 import com.aacademy.moonlight.dto.car.CarResponse;
 import com.aacademy.moonlight.dto.car.CarTransferResponse;
 import com.aacademy.moonlight.dto.user.UserResponse;
+import com.aacademy.moonlight.entity.hotel.RoomReservation;
+import com.aacademy.moonlight.service.hotel.RoomReservationService;
 import com.aacademy.moonlight.service.car.CarTransferService;
 import com.aacademy.moonlight.entity.hotel.RoomReservation;
 import com.aacademy.moonlight.service.hotel.RoomReservationService;
@@ -39,6 +41,9 @@ public class AdminController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    RoomReservationService roomReservationService;
 
     @Autowired
     CarTransferService carTransferService;
@@ -96,6 +101,11 @@ public class AdminController {
     @GetMapping(path = "/user-by-phoneNumber/{number}")
     public ResponseEntity<UserResponse> getUserByPhoneNumber(@PathVariable String number) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findUserByPhoneNumber(number));
+    }
+
+    @GetMapping(path = "/getAllRoomReservations")
+    public ResponseEntity<List<RoomReservation>> getAllRoomReservations(){
+        return ResponseEntity.status(HttpStatus.OK).body(roomReservationService.getAllRoomReservations());
     }
 
     @GetMapping(path = "/allCarTransfers")
