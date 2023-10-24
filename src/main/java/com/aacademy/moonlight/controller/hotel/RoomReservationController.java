3f46bf4593d589133bd0e34f732bd70983b2bd2c
AlexLazrag <alex.lazrag@gmail.com>
2023-10-24 19:54:18 +0300

    Final version of room reservation

--- src/main/java/com/aacademy/moonlight/controller/hotel/RoomReservationController.java
+++ src/main/java/com/aacademy/moonlight/controller/hotel/RoomReservationController.java
@@ -1,6 +1,8 @@
 package com.aacademy.moonlight.controller.hotel;
 
+import com.aacademy.moonlight.converter.hotel.RoomReservationConverter;
 import com.aacademy.moonlight.dto.hotel.RoomReservationRequest;
+import com.aacademy.moonlight.dto.hotel.RoomReservationResponse;
 import com.aacademy.moonlight.entity.hotel.RoomReservation;
 import com.aacademy.moonlight.service.hotel.RoomReservationService;
 import jakarta.validation.Valid;
@@ -15,12 +17,15 @@ public class RoomReservationController {
 
     @Autowired
     RoomReservationService service;
+    @Autowired
+    RoomReservationConverter converter;
 
     @PostMapping("/create-room-reservation")
-    public ResponseEntity<String> createRoomReservation(@Valid @RequestBody RoomReservationRequest request){
+    public ResponseEntity<RoomReservationResponse> createRoomReservation(@Valid @RequestBody RoomReservationRequest request){
         RoomReservation reservation = service.createRoomReservation(request);
-        String response = String.format("Your reservation has been created");
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        RoomReservationResponse response = converter.toResponse(reservation);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{roomId}/get-all-reservations")
---
+++ src/main/java/com/aacademy/moonlight/converter/hotel/RoomReservationConverter.java
@@ -0,0 +1,17 @@
+package com.aacademy.moonlight.converter.hotel;
+
+import com.aacademy.moonlight.dto.hotel.RoomReservationResponse;
+import com.aacademy.moonlight.entity.hotel.RoomReservation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoomReservationConverter {
+
+    public RoomReservationResponse toResponse(RoomReservation savedReservation){
+        return new RoomReservationResponse(
+                savedReservation.getRoom().getId(),
+                savedReservation.getStartDate(),
+                savedReservation.getEndDate()
+        );
+    }
+}
