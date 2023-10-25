f2e30031d0491b87bba8229dad531b698ba66d92
Daniela Petrova <dani055@abv.bg>
2023-10-25 20:25:00 +0300

    JB23AT2-41-Conflict-resolved

--- src/main/java/com/aacademy/moonlight/security/config/SecurityConfig.java
+++ src/main/java/com/aacademy/moonlight/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.aacademy.moonlight.security.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
