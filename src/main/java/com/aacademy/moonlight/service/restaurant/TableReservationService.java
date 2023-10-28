4f51fc120f01041d5e1880357e46d6527f2c5900
AlexLazrag <alex.lazrag@gmail.com>
2023-10-28 13:34:56 +0300

    FInd Room Reservation for user by ID

--- src/main/java/com/aacademy/moonlight/service/restaurant/TableReservationService.java
+++ src/main/java/com/aacademy/moonlight/service/restaurant/TableReservationService.java
@@ -14,5 +14,5 @@ public interface TableReservationService {
     void deleteById(Long id);
     List<TableReservationResponse> getAllTableReservations();
     List<TableReservationResponse> getTableReservationsByUser();
-    TableReservationResponse getTableReservationById(Long id);
+    TableReservationResponse getPersonalTableReservationById(Long id);
 }
